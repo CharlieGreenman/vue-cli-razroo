07ab2c8a052c096b8fc64bc17279bcf18574b2b7
Charlie Greenman <charlie@razroo.com>
2018-01-03 13:21:08 -0500

    Add some content to the vue ShowHide component.

---
+++ src/components/ShowHide.vue
@@ -0,0 +1,14 @@
+<div>This is the show hide part of the app.</div>
+
+<button (click)="toggle$.next()" class="button">toggle</button>
+
+<script>
+export default {
+  name: 'ShowHide',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
